26816a621a84d31439d1daa1bbc82c926f185042
Md. Nashid Kamal <nashidkamaljitu7@gmail.com>
2020-03-01 01:40:13 +0600

    Scanning methods from controllers

--- src/com/framework/controller/ControllerInitializer.java
+++ src/com/framework/controller/ControllerInitializer.java
@@ -3,6 +3,8 @@ package com.framework.controller;
 import com.framework.log.AppLogger;
 import org.reflections.Reflections;
 
+import java.lang.reflect.Method;
+
 public class ControllerInitializer {
     public static void loadControllers() {
         AppLogger.infoMessage(ControllerInitializer.class, "Scanning classpath for Controllers");
@@ -12,6 +14,13 @@ public class ControllerInitializer {
             Controller controller = cl.getAnnotation(Controller.class);
             AppLogger.developmentMessage(ControllerInitializer.class, "Controller loaded -> Name: " + cl.getName() + " Mapping: " + controller.mapping());
 
+            Method [] methodList = cl.getMethods();
+            for (Method method : methodList) {
+                Mapping mapping = method.getAnnotation(Mapping.class);
+                if (mapping != null && !mapping.mapping().isEmpty()) {
+                    AppLogger.developmentMessage(ControllerInitializer.class, "Controller -> Method loaded -> Class: " + cl.getName() + " Name: " + method.getName() + " Mapping: " + mapping.mapping());
+                }
+            }
         }
         AppLogger.infoMessage(ControllerInitializer.class, "Controller loading finished.");
     }
